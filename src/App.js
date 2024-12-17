c6d2852e1de6f94194e8618d5835d76bdc782485
Mateo Ramos <mateomramos1@gmail.com>
2024-12-17 08:55:17 -1000

    base layout

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
 import './App.css';
+import placeholder from './GamePlaceholder.jpg';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <head>
+        <title>IT Chess</title>
+      </head>
+
+      <header className="header">
+        <h1>InFoMaChess</h1>
+
       </header>
+
+      <div className='flexContainer'>
+        <aside className="aside">
+          <h1>Aside</h1>
+        </aside>
+
+        <section className="body">
+          <img src={placeholder}></img>
+        </section>
+      </div>
+
+      <footer className="footer">
+        <h5>by Ivan, Ferdinand, and Mateo</h5>
+        <a href='https://github.com/mateomramos/ITSeniorProject'>GitHub</a>
+      </footer>
     </div>
   );
 }
