131af646eb64a8803235ec745228b2da864a617f
mateomramos <mateomramos1@gmail.com>
2025-01-09 09:49:40 -0500

    fixing errors on chessboard scipt

---
+++ game/Assets/Scripts/ChessboardSC.cs
@@ -0,0 +1,32 @@
+using System;
+using UnityEngine;
+
+public class ChessboardSC : MonoBehavior
+{
+    // LOGIC 
+    private const int TILE_COUNT_X = 8;
+    private const int TILE_COUNT_Y = 8;
+    private GameObject[,] tiles;
+    
+    private void awake() {
+        GenerateAllTiles(1, TILE_COUNT_X, TILE_COUNT_Y);
+    }
+
+    private void GenerateAllTiles(float tileSize, int tileCountX, int tileCountY)
+    {
+        tiles = new GameObject[tileCountX, tileCountY];
+
+        for (int x = 0; x < tileCountX; x++) {
+            for (int y = 0; y < tileCountY; y++) {
+                tiles[x, y] = GenerateSingleTile(tileSize, x, y);
+            }
+        }
+    }
+    private GameObject GenerateSingleTile(float tilesize, int x, int y)
+    {
+        GameObject tileObject = new GameObject(string.Format("X:{0}, Y:{1}", x, y));
+        tileObject.transform.parent = transform;
+
+        return tileObject;
+    }
+}
