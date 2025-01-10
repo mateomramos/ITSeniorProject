f5ca409f0078beff3f6c29b18d4aefe90b3fb1df
mateomramos <mateomramos1@gmail.com>
2025-01-10 14:11:43 -0500

    chess board generation script

--- game/Assets/Scripts/Chessboard.cs
+++ game/Assets/Scripts/Chessboard.cs
@@ -3,13 +3,16 @@ using UnityEngine;
 
 public class Chessboard : MonoBehaviour
 {
+
+    [Header("Art stuff")]
+    [SerializeField] private Material tileMaterial;
+
     // LOGIC 
     private const int TILE_COUNT_X = 8;
     private const int TILE_COUNT_Y = 8;
     private GameObject[,] tiles;
-    private Transform transform;
 
-    private void awake() {
+    private void Awake() {
         GenerateAllTiles(1, TILE_COUNT_X, TILE_COUNT_Y);
     }
 
@@ -27,6 +30,23 @@ public class Chessboard : MonoBehaviour
     {
         GameObject tileObject = new GameObject(string.Format("X:{0}, Y:{1}", x, y));
         tileObject.transform.parent = transform;
+
+        Mesh mesh = new Mesh();
+        tileObject.AddComponent<MeshFilter>().mesh = mesh;
+        tileObject.AddComponent<MeshRenderer>().material = tileMaterial;
+
+        Vector3[] vertices = new Vector3[4];
+        vertices[0] = new Vector3(x * tilesize, 0, y * tilesize);
+        vertices[1] =new Vector3(x * tilesize, 0 , (y+1) * tilesize);
+        vertices[2] = new Vector3((x + 1) * tilesize, 0, y * tilesize);
+        vertices[3] = new Vector3((x + 1) * tilesize, 0, (y + 1) * tilesize);
+
+        int[] tris = new int[] { 0, 1, 2, 1, 3, 2 };
+
+        mesh.vertices = vertices;
+        mesh.triangles = tris;
+
+        tileObject.AddComponent<BoxCollider>();
 
         return tileObject;
     }
